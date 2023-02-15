caa099ee10b6e784e2a4c3364d3dc2250717eab4
monicamrdam <monicamrdam@gmail.com>
2023-02-15 19:39:10 +0100

    metodos clase ControladorPrestamos

---
+++ biblioteca_nivel1/ControladorPrestamos.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class ControladorPrestamos here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ControladorPrestamos
+{
+   private static final String  NOMBRE_ARCHIVO ="prestamo.txt";
+   private static final String SEPARADOR_CAMPO= ";";
+   private static final String SEPARADOR_REGISTRO = "\n";
+   
+   public static void registrar (Prestamo prestamo){
+       
+   }
+   public static ArrayList <Prestamo> obtener(){
+       ArrayList <Prestamo> prestamos = new ArrayList<>();
+       return prestamos;
+   }
+   public static int cantidadLibrosPrestados (String numeroSocio, ArrayList<Prestamo> prestamos){
+        int i= -1;
+        return i;
+   }
+   public static void imprimirPrestamos(ArrayList<Prestamo> prestamos){
+       
+   }
+   public static void solicitarDatosYCrearPrestamos(){
+       
+   }
+}
