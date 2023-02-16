44607b0b89fcbc1ee6640c260bccc4430c7c554f
monicamrdam <monicamrdam@gmail.com>
2023-02-16 13:24:16 +0100

    update desarrollo metodos

--- biblioteca_nivel2/ControlPrestamos.java
+++ biblioteca_nivel2/ControlPrestamos.java
@@ -1,27 +1,86 @@
 import java.util.ArrayList;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.time.*;
+import java.time.format.*;
 
 /**
  * Write a description of class ControlPrestamos here.
- * Clase controlar los usuarios
+ * Clase controlar los prestamos de libros a los usuarios
  * @author (monicamrmda) 
  * @version (16/02/2023)
  */
 public class ControlPrestamos
 {
-    public boolean registrar(Prestamos prestamo){
-       boolean res=true;
-       return res;
+    //Declaramos las variables que usaremos en el archivo.tx
+    private static final String NOMBRE_ARCHIVO ="prestamos.txt";
+    private static final String SEPARADOR_CAMPO=";";
+    private static final String SEPARADOR_REGISTRO= "\n";
+    
+    /*Utilizaremos tres métodos para registrar, listar
+    ControladorPrestamos.imprimirPrestamos(ControladorPrestamos.obtener());
+    ControladorPrestamos.solicitarDatosYCrearPrestamo(); */
+    public static void registrar(Prestamo prestamo){
+       ControlLibros.marcarComoPrestado(prestamo.getCodigoLibro());
+       try {
+            BufferedWriter bufW = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO, true));
+            bufW.write(prestamo.getCodigoLibro() + SEPARADOR_CAMPO + prestamo.getNumeroSocio() + SEPARADOR_CAMPO + prestamo.getFechaFormateada() + SEPARADOR_REGISTRO);
+            bufW.close();
+        }catch (IOException e) {
+            System.out.println("Error al registrar el prestamo: " + e.getMessage());
+        }
     }
-   public boolean actualizar(Prestamos prestamo){
-       boolean res=true;
-       return res;
-    }
-   public ArrayList Prestamos (String prestamos){
-       ArrayList <Prestamos> lista = new ArrayList<>();
-       return lista;
+   public static ArrayList <Prestamo> obtener(){
+       ArrayList <Prestamo> prestamos = new ArrayList<>();
+       FileReader fileReader=null;
+       BufferedReader bufR=null;
+        try {
+            fileReader= new FileReader(NOMBRE_ARCHIVO);
+            bufR= new BufferedReader(fileReader);
+            String linea;
+            while((linea = bufR.readLine())!=null){
+                String [] arrayPrestamo= linea.split(SEPARADOR_CAMPO);
+                prestamos.add (new Prestamo(arrayPrestamo[0], arrayPrestamo[1], LocalDateTime.parse(arrayPrestamo[2], new DateTimeFormatterBuilder().parseCaseInsensitive().append(DateTimeFormatter.ofPattern ("yyy--MM--dd HH:mm:ss")).toFormatter())));
+            }
+        } catch (IOException e) {
+            System.out.println("Excepción leyendo archivo: " + e.getMessage());
+        } finally {
+            try {
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+                if (bufR != null) {
+                    bufR.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Excepción cerrando: " + e.getMessage());
+            }
+        }
+       return prestamos;
    }
-   public boolean eliminar(){
-         boolean res=true;
-        return res;
-}
+    public static void solicitarDatosYCrearPrestamo() {
+        Libro libro = ControlLibros.imprimirLibrosYPedirSeleccion();
+        Usuarios usuario = ControlUsuarios.imprimirSociosYPedirSeleccion();
+        ControlPrestamos.registrar(new Prestamo(libro.getCodigo(), usuario.getNumero(), LocalDateTime.now()));
+        System.out.println("Prestamo registrado correctamente");
+    }
+    public static void imprimirPrestamos(ArrayList<Prestamo> prestamos) {
+        System.out
+                .println("+-----+------------------------------+------------------------------+--------------------+");
+        System.out.printf("|%-5s|%-30s|%-30s|%-20s|\n", "No", "Codigo libro", "No. Socio", "Fecha");
+        System.out
+                .println("+-----+------------------------------+------------------------------+--------------------+");
+
+        for (int x = 0; x < prestamos.size(); x++) {
+            Prestamo prestamo = prestamos.get(x);
+            System.out.printf("|%-5d|%-30s|%-30s|%-20s|\n", x, prestamo.getCodigoLibro(), prestamo.getNumeroSocio(),
+                    prestamo.getFechaFormateada());
+            System.out.println(
+                    "+-----+------------------------------+------------------------------+--------------------+");
+        }
+    }
+    
 }
