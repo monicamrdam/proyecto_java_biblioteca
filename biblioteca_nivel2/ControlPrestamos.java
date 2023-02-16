c03c662b7e74f3b9b8ff25560e7972b095f94f97
monicamrdam <monicamrdam@gmail.com>
2023-02-16 06:20:23 +0100

    añadido  clase ControlPrestamos

---
+++ biblioteca_nivel2/ControlPrestamos.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class ControlPrestamos here.
+ * Clase controlar los usuarios
+ * @author (monicamrmda) 
+ * @version (16/02/2023)
+ */
+public class ControlPrestamos
+{
+    public boolean registrar(Prestamos prestamo){
+       boolean res=true;
+       return res;
+    }
+   public boolean actualizar(Prestamos prestamo){
+       boolean res=true;
+       return res;
+    }
+   public ArrayList Prestamos (String prestamos){
+       ArrayList <Prestamos> lista = new ArrayList<>();
+       return lista;
+   }
+   public boolean eliminar(){
+         boolean res=true;
+        return res;
+}
+}
