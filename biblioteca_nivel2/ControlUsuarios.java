e635054778c726d1631a3c766b973c8fef882372
monicamrdam <monicamrdam@gmail.com>
2023-02-16 06:21:42 +0100

    añadida clase ControlUsuarios

---
+++ biblioteca_nivel2/ControlUsuarios.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class ControlUsuarios here.
+ * Clase controlar los usuarios
+ * @author (monicamrmda) 
+ * @version (16/02/2023)
+ */
+public class ControlUsuarios
+{
+    public boolean registrar(Usuarios usuario){
+       boolean res=true;
+       return res;
+    }
+   public boolean actualizar(Usuarios usuario){
+       boolean res=true;
+       return res;
+    }
+   public ArrayList Usuarios (String usuarios){
+       ArrayList <Usuarios> lista = new ArrayList<>();
+       return lista;
+   }
+   public boolean eliminar(){
+     boolean res=true;
+       return res;
+    }
+}
