93d4dfd485ff846ce3d8a0d1293278f81bc632fa
monicamrdam <monicamrdam@gmail.com>
2023-02-16 11:01:28 +0100

    update clase Biblioteca incorporado switch

--- biblioteca_nivel2/Biblioteca.java
+++ biblioteca_nivel2/Biblioteca.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Write a description of class Biblioteca here.
@@ -8,6 +9,38 @@
 public class Biblioteca
 {
     public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String opcion="";
+       
+        do{
+            System.out.println("1. Registrar socio\n2. Ver socio\n3. Registrar libro\n4. Ver libro\n5. Registrar prestamo\n6. Ver prestamos\n0. Salir\nElige:");
+            opcion=sc.nextLine();
+            switch(opcion){
+            case "1":
+                System.out.println("Has seleccionado registrar socio");
+                break;
+            case "2":
+                System.out.println("Has seleccionado ver socio");
+                break;
+            case "3":
+                System.out.println("Has seleccionado registrar libro");
+                break;
+            case "4":
+                System.out.println("Has seleccionado ver libro");
+                break;
+            case "5":
+                System.out.println("Has seleccionado registrar prestamo");
+                break;
+            case "6":
+                System.out.println("Has seleccionado ver prestamos");
+                break;
+            case "0":
+                break;
+            default:
+                System.out.println("Has seleccionado una opción incorrecta");
+            }
+        }while(!opcion.equals("0"));
+        System.out.println("Programa finalizado");
         
     }
 }
